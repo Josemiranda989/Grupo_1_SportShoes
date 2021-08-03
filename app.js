e1b98e0a1c092b8c66342e668c07bd8682bb62c5
gabikohen <gabokohen@gamil.com>
2021-08-03 18:16:53 -0300

    Primeros pasos

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express=require('express');
+const { dirname } = require('path');
 const app=express ();
 const path=require('path');
 
+app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views/index.html'))
 });
+
 
 
 app.listen(3002,()=>{
