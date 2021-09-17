010dd367c13e1b9ddcc75ff50657eaae6cb1d2e6
gabikohen <gabokohen@gmail.com>
2021-09-17 19:22:55 -0300

    tucuman 1

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,12 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController')
 
-
+//FORMULARIO DE LOGIN
 router.get('/login', userController.login);
+//FORMULARIO DE REGISTRO
 router.get('/register', userController.register);
+
+//PERFIL DE USUARIO 
+router.get('/profile/:userId',userController.profile);
 
 module.exports = router;
