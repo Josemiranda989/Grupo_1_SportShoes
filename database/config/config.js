3d55b401684120ad7623e83f4757cfa125404677
José Miranda <84145112+Josemiranda989@users.noreply.github.com>
2021-10-27 23:49:28 -0300

    Se actualizó nombre de DB en la configuración

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "SPORT_DB",
+    "database": "SportShoes_DB",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
