aee41920e0e51f8326894dfea634d978c0a1cd4a
José Miranda <josemiranda989@gmail.com>
2021-12-13 23:51:08 -0300

    focus en validaciones

--- public/javascripts/validationCreate.js
+++ public/javascripts/validationCreate.js
@@ -8,19 +8,70 @@ function validarFormulario(evento) {
   evento.preventDefault();
 
   /* productName */
+  var productNameInput = document.getElementById("productName")
   var productName = document.getElementById("productName").value;
   var errProductName = document.getElementById("errProductName");
   if (productName.length == 0) {
     errProductName.classList.add("show");
     errProductName.classList.remove("hidden");
     errProductName.innerHTML = "You must enter your name F";
+    productNameInput.focus()
     return;
   } else {
     errProductName.classList.remove("show");
     errProductName.classList.add("hidden");
   }
   
+  /* price */
+  var priceInput = document.getElementById("price")
+  var price = document.getElementById("price").value;
+  errPrice = document.getElementById("errPrice");
+
+  if (price === "") {
+    errPrice.classList.add("show");
+    errPrice.classList.remove("hidden");
+    errPrice.innerHTML = "Enter Product price F";
+    priceInput.focus()
+    return;
+  } else {
+    errPrice.classList.remove("show");
+    errPrice.classList.add("hidden");
+  }
+
+  /* size */
+  var sizeInput = document.getElementById("size")
+  var size = document.getElementById("size").value;
+  errSize = document.getElementById("errSize");
+
+  if (size === "") {
+    errSize.classList.add("show");
+    errSize.classList.remove("hidden");
+    errSize.innerHTML = "Enter Product size F";
+    sizeInput.focus()
+    return;
+  } else {
+    errSize.classList.remove("show");
+    errSize.classList.add("hidden");
+  }
+
+  /* colors */
+  var colorInput = document.getElementById("color")
+  var color = document.getElementById("color").value;
+  errColor = document.getElementById("errColor");
+
+  if (color === "") {
+    errColor.classList.add("show");
+    errColor.classList.remove("hidden");
+    errColor.innerHTML = "Enter Product color F";
+    colorInput.focus()
+    return;
+  } else {
+    errColor.classList.remove("show");
+    errColor.classList.add("hidden");
+  }
+
   /* Brand */
+  var brandInput = document.getElementById("brand")
   var brand = document.getElementById("brand").value;
   errBrand = document.getElementById("errBrand");
 
@@ -28,13 +79,15 @@ function validarFormulario(evento) {
     errBrand.classList.add("show");
     errBrand.classList.remove("hidden");
     errBrand.innerHTML = "Enter Product Brand F";
+    brandInput.focus()
     return;
   } else {
-    errBrand.classList.add("show");
-    errBrand.classList.remove("hidden");
+    errBrand.classList.remove("show");
+    errBrand.classList.add("hidden");
   }
 
   /* Avatar */
+  var img1Input = document.getElementById("img1")
   var img1 = document.getElementById("img1").value;
   errImg1 = document.getElementById("errImg1");
   var allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
@@ -43,6 +96,7 @@ function validarFormulario(evento) {
     errImg1.classList.remove("hidden");
     errImg1.innerHTML =
       "Please upload file having extensions .jpeg/.jpg/.png/.gif only.";
+    img1Input.focus()
     return;
   } else {
     errImg1.classList.remove("show");
@@ -50,7 +104,7 @@ function validarFormulario(evento) {
   }
 
   /* Description */
-
+  var descriptionInput = document.getElementById("description")
   var description = document.getElementById("description").value;
   errDescription = document.getElementById("errDescription");
       /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/.test(description);
@@ -59,6 +113,7 @@ function validarFormulario(evento) {
     errDescription.classList.remove("hidden");
     errDescription.innerHTML =
       "The description must be greater than 20 characters F";
+    descriptionInput.focus()
     return;
   } else {
     errDescription.classList.remove("show");
--- public/javascripts/validationRegister.js
+++ public/javascripts/validationRegister.js
@@ -8,31 +8,81 @@ function validarFormulario(evento) {
   evento.preventDefault()
 
   /* FullName */
-  var fullName = document.getElementById('fullName').value
- var  errFullName = document.getElementById('errFullName')
+  const fullNameInput = document.getElementById('fullName')
+  const fullName = document.getElementById('fullName').value
+  const errFullName = document.getElementById('errFullName')
   if (fullName.length == 0) {
     errFullName.classList.add('show')
     errFullName.classList.remove('hidden')
     errFullName.innerHTML = 'You must enter your name F'
+    fullNameInput.focus()
     return
   } else {
     errFullName.classList.remove('show')
     errFullName.classList.add('hidden')
   }
 
+  /* UserName */
+  const userNameInput = document.getElementById('userName')
+  const userName = document.getElementById('userName').value
+  const errUserName = document.getElementById('errUserName')
+  if (userName.length == 0) {
+    errUserName.classList.add('show')
+    errUserName.classList.remove('hidden')
+    errUserName.innerHTML = 'You must enter your name F'
+    userNameInput.focus()
+    return
+  } else {
+    errUserName.classList.remove('show')
+    errUserName.classList.add('hidden')
+  }
+
+  /* Country */
+  const countryInput = document.getElementById('country')
+  const country = document.getElementById('country').value
+  const errCountry = document.getElementById('errCountry')
+  if (country.length == 0) {
+    errCountry.classList.add('show')
+    errCountry.classList.remove('hidden')
+    errCountry.innerHTML = 'Select a country'
+    countryInput.focus()
+    return
+  } else {
+    errCountry.classList.remove('show')
+    errCountry.classList.add('hidden')
+  }
+
+  /* Address */
+  const addressInput = document.getElementById('address')
+  const address = document.getElementById('address').value
+  const errAddress = document.getElementById('errAddress')
+  if (address.length == 0) {
+    errAddress.classList.add('show')
+    errAddress.classList.remove('hidden')
+    errAddress.innerHTML = 'Select a Address'
+    addressInput.focus()
+    return
+  } else {
+    errAddress.classList.remove('show')
+    errAddress.classList.add('hidden')
+  }
+
   /* Email */
-  var email = document.getElementById('email').value
+  const emailInput = document.getElementById('email')
+  const email = document.getElementById('email').value
   errEmail = document.getElementById('errEmail')
   const charEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)+\.\S+/.test(email)
   if (email === '') {
     errEmail.classList.add('show')
     errEmail.classList.remove('hidden')
     errEmail.innerHTML = 'Enter your email F'
+    emailInput.focus()
     return
   } else if (!charEmail) {
     errEmail.classList.add('show')
     errEmail.classList.remove('hidden')
     errEmail.innerHTML = 'Invalid format F'
+    emailInput.focus()
     return
   } else {
     errEmail.classList.remove('show')
@@ -40,14 +90,16 @@ function validarFormulario(evento) {
   }
 
   /* Avatar */
-  var avatar = document.getElementById('avatar').value
+  const avatarInput = document.getElementById('avatar')
+  const avatar = document.getElementById('avatar').value
   errAvatar = document.getElementById('errAvatar')
-  var allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i
+  const allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i
   if (!allowedExtensions.exec(avatar)) {
     errAvatar.classList.add('show')
     errAvatar.classList.remove('hidden')
     errAvatar.innerHTML =
       'Please upload file having extensions .jpeg/.jpg/.png/.gif only.'
+    avatarInput.focus()
     return
   } else {
     errAvatar.classList.remove('show')
@@ -55,8 +107,9 @@ function validarFormulario(evento) {
   }
 
   /* Password */
-  var password = document.getElementById('password').value
-   var errPassword = document.getElementById('errPassword')
+  const passwordInput = document.getElementById('password')
+  const password = document.getElementById('password').value
+  const errPassword = document.getElementById('errPassword')
   const charPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/.test(
     password,
   )
@@ -64,28 +117,32 @@ function validarFormulario(evento) {
     errPassword.classList.add('show')
     errPassword.classList.remove('hidden')
     errPassword.innerHTML = 'The password must be greater than 8 characters F'
+    passwordInput.focus()
     return
   } else if (!charPass) {
     errPassword.classList.add('show')
     errPassword.classList.remove('hidden')
     errPassword.innerHTML =
       'The password must contain uppercase, lowercase, number and special character F'
+    passwordInput.focus()
     return
   } else {
     errPassword.classList.remove('show')
     errPassword.classList.add('hidden')
   }
 
-  var rePassword = document.getElementById('rePassword').value
-  var errRePassword = document.getElementById('errRePassword')
-  
-  if(rePassword != password) {
+  /* rePassword */
+  const rePasswordInput = document.getElementById('rePassword')
+  const rePassword = document.getElementById('rePassword').value
+  const errRePassword = document.getElementById('errRePassword')
+
+  if (rePassword != password) {
     errRePassword.classList.add('show')
     errRePassword.classList.remove('hidden')
     errRePassword.innerHTML = 'The password does not match'
+    rePasswordInput.focus()
     return
-   
-  }else{
+  } else {
     errRePassword.classList.remove('show')
     errRePassword.classList.add('hidden')
   }
